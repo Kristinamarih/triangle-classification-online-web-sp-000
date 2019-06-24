4a8d7629ebce207c13297dac4a20956c06943fdf
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-24 11:06:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   
   def kind()
-    if (@length_1 != @length_2 || @length_3) && (@length_2 != @length_3)
+    if (@length_1 <= 0) || (@length_2 <= 0) || (@length_2 != @length_3)
       return @scalene
     elsif (@length_1 == @length_2 || @length_3) || (@length_2 == @length_3)
     return @isosceles
