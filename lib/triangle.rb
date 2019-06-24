01295505bc5190795a3c24887c9b98c870c8807d
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-24 11:12:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       raise TriangleError
     elsif (@length_1+@length_2 <= @length_3) || (@length_1+@length_3 <= @length_2) || (@length_2+@length_3 <= @length_1)
       raise TriangleError
-    elsif (@length_3 == @length_2) && (@length_2 == @length_1)
+    elsif (@length_3 == @length_2) && (@length_2 == @length_1) && (@length_1 == @length_3)
     return @equilateral
   else
     raise TriangleError
