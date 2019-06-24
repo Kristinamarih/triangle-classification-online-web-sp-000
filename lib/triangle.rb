80d4d6f3881e77a87d51363d115fca5d15e5970d
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-24 10:55:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       return @scalene
     elsif (length_1 == length_2 || length_3) || (length_2 == length_3)
     return @isosceles
-    elsif (length_3 == length_2 == length_1)
+    elsif (length_3 == length_2) && (length_2 == length_1)
     return @equilateral
   else
     raise TriangleError
